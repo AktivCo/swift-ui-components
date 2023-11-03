3474b0b654d92251fe07894c4d730f59bfd50383
Ivan Poderegin <poderegin@rutoken.ru>
2023-11-03 16:47:38 +0300

    Add colors
    Resolve: #9

--- Sources/rt-ui-components/RtColors.swift
+++ Sources/rt-ui-components/RtColors.swift
@@ -12,5 +12,10 @@ public extension Color {
     enum RtColors {
         public static let rtColorsSecondary = Color(UIColor(named: "rtColorsSecondary", in: .module, compatibleWith: nil)!)
         public static let rtColorsPrimary100 = Color(UIColor(named: "rtColorsPrimary100", in: .module, compatibleWith: nil)!)
+        public static let rtColorsPrimary90 = Color(UIColor(named: "rtColorsPrimary90", in: .module, compatibleWith: nil)!)
+        public static let rtColorsOnPrimary2 = Color(UIColor(named: "rtColorsOnPrimary2", in: .module, compatibleWith: nil)!)
+        public static let rtColorsSystemBlue = Color(UIColor(named: "rtColorsSystemBlue", in: .module, compatibleWith: nil)!)
+        public static let rtColorsSystemGreen = Color(UIColor(named: "rtColorsSystemGreen", in: .module, compatibleWith: nil)!)
+        public static let rtColorsSystemRed = Color(UIColor(named: "rtColorsSystemRed", in: .module, compatibleWith: nil)!)
     }
 }
