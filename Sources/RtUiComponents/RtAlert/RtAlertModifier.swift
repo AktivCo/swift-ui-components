a31bb7dcdb126bbc123caf8f57f84a1afedc9949
Nikita Deviatykh <deviatykh@rutoken.ru>
2023-12-25 12:02:51 +0300

    Fix RtAlert
    This commit fixes button hitbox and switches Binding to RtAlertModel
    
    Resolve: #16
    Resolve: #19

--- Sources/RtUiComponents/RtAlert/RtAlertModifier.swift
+++ Sources/RtUiComponents/RtAlert/RtAlertModifier.swift
@@ -9,51 +9,41 @@ import SwiftUI
 
 
 public extension View {
-    func rtAlert(isPresented: Binding<Bool>, alertData: RtAlertModel) -> some View {
-        return self.modifier(RtAlertModifier(isPresented: isPresented, title: alertData.title,
-                                             subtitle: alertData.subTitle, buttons: alertData.buttons))
+    func rtAlert(alertModel: Binding<RtAlertModel?>) -> some View {
+        return self.modifier(RtAlertModifier(alertModel: alertModel))
     }
 }
 
 private struct RtAlertModifier: ViewModifier {
-    @Binding var isPresented: Bool
-    var title: RtAlertTitle
-    var subtitle: String?
-    var buttons: [RtAlertButtonData]
+    @Binding var alertModel: RtAlertModel?
 
     public func body(content: Content) -> some View {
-        RtAlertView(isShowing: $isPresented, title: title, subTitle: subtitle,
-                    buttons: buttons, presentationView: content)
+        RtAlertView(alertModel: $alertModel, presentationView: content)
     }
 }
 
 private struct AlertContentView: View {
-    @State var isAlertShown = false
-    @State var alertInfo = RtAlertModel(title: .titleOnly(""), buttons: [])
+    @State var alertInfo: RtAlertModel?
     var body: some View {
         VStack(spacing: 10) {
             Button("Toggle success alert") {
                 alertInfo = RtAlertModel(
                     title: .success("Подпись верна"),
                     buttons: [.init(.regular("ОК"))])
-                isAlertShown.toggle()
             }
             Button("Toggle failure alert") {
                 alertInfo = RtAlertModel(
                     title: .failure("Подпись неверна"),
                     subTitle: "И не удалось построить цепочку доверия для сертификата",
                     buttons: [.init(.regular("ОК"))])
-                isAlertShown.toggle()
             }
             Button("Toggle info alert") {
-                isAlertShown.toggle()
                 alertInfo = RtAlertModel(
                     title: .titleOnly("Потеряно соединение с Рутокеном"),
                     subTitle: "Повторите подключение и не убирайте Рутокен до завершения обмена данными",
                     buttons: [.init(.bold("ОК"))])
             }
             Button("Toggle many buttons alert") {
-                isAlertShown.toggle()
                 alertInfo = RtAlertModel(
                     title: .titleOnly("Потеряно соединение с Рутокеном"),
                     subTitle: "Повторите подключение и не убирайте Рутокен до завершения обмена данными",
@@ -64,10 +54,7 @@ private struct AlertContentView: View {
             }
             .padding(.bottom, 300)
         }
-        .rtAlert(
-            isPresented: $isAlertShown,
-            alertData: alertInfo
-        )
+        .rtAlert(alertModel: $alertInfo)
     }
 }
 
--- Sources/RtUiComponents/RtAlert/RtAlertView.swift
+++ Sources/RtUiComponents/RtAlert/RtAlertView.swift
@@ -9,23 +9,20 @@ import SwiftUI
 
 
 struct RtAlertView<Presenter>: View where Presenter: View {
-    @Binding var isShowing: Bool
-    let title: RtAlertTitle
-    let subTitle: String?
-    let buttons: [RtAlertButtonData]
+    @Binding var alertModel: RtAlertModel?
     let presentationView: Presenter
 
     var body: some View {
         ZStack {
-            presentationView.disabled(isShowing)
+            presentationView.disabled(alertModel != nil)
             Color.RtColors.rtIosElementsAlertOverlay
                 .edgesIgnoringSafeArea(.all)
-                .opacity(self.isShowing ? 1 : 0)
+                .opacity(alertModel == nil ? 0 : 1)
 
             VStack(spacing: 0) {
                 VStack(spacing: 0) {
-                    title.content
-                    if let subTitle {
+                    alertModel?.title.content
+                    if let subTitle = alertModel?.subTitle {
                         Text(subTitle)
                             .padding(.top, 5)
                             .font(.system(size: 13))
@@ -36,23 +33,25 @@ struct RtAlertView<Presenter>: View where Presenter: View {
                 .padding(.horizontal, 16)
                 .padding(.top, 19)
                 .padding(.bottom, 17)
-                ForEach(0..<buttons.count, id: \.self) { counter in
-                    Divider()
-                        .overlay(Color.RtColors.rtIosElementsAlertSeparator)
-                    Button(action: {
-                        buttons[counter].action?()
-                        isShowing = false
-                    }, label: {
-                        buttons[counter].title.text
-                    })
-                        .frame(height: 44)
+                if let buttons = alertModel?.buttons {
+                    ForEach(0..<buttons.count, id: \.self) { counter in
+                        Divider()
+                            .overlay(Color.RtColors.rtIosElementsAlertSeparator)
+                        Button {
+                            buttons[counter].action?()
+                            alertModel = nil
+                        } label: {
+                            buttons[counter].title.text
+                                .frame(width: 270, height: 44)
+                        }
                         .multilineTextAlignment(.center)
+                    }
                 }
             }
             .background(Color.RtColors.rtIosElementsAlertSurface)
             .background(.ultraThinMaterial)
             .cornerRadius(15)
-            .opacity(self.isShowing ? 1 : 0)
+            .opacity(alertModel == nil ? 0 : 1)
             .frame(width: 270)
         }
         .edgesIgnoringSafeArea(.all)
