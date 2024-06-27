5351a0e9c8c45f59e6abee7967bd8731bb15ae65
Nikita Deviatykh <deviatykh@rutoken.ru>
2024-06-27 12:58:37 +0300

    Add alert appearance and disappearance animation
    Resolve: #36

--- Sources/RtUiComponents/RtAlert/RtAlertView.swift
+++ Sources/RtUiComponents/RtAlert/RtAlertView.swift
@@ -18,42 +18,46 @@ struct RtAlertView<Presenter>: View where Presenter: View {
             Color.RtColors.rtIosElementsAlertOverlay
                 .edgesIgnoringSafeArea(.all)
                 .opacity(alertModel == nil ? 0 : 1)
+                .animation(.easeOut(duration: 0.24), value: alertModel == nil)
 
-            VStack(spacing: 0) {
+            if alertModel != nil {
                 VStack(spacing: 0) {
-                    alertModel?.title.content
-                    if let subTitle = alertModel?.subTitle {
-                        Text(subTitle)
-                            .padding(.top, 5)
-                            .font(.system(size: 13))
-                            .foregroundColor(.primary.opacity(0.5))
-                            .multilineTextAlignment(.center)
-                    }
-                }
-                .padding(.horizontal, 16)
-                .padding(.top, 19)
-                .padding(.bottom, 17)
-                if let buttons = alertModel?.buttons {
-                    ForEach(0..<buttons.count, id: \.self) { counter in
-                        Divider()
-                            .overlay(Color.RtColors.rtIosElementsAlertSeparator)
-                        Button {
-                            buttons[counter].action?()
-                            alertModel = nil
-                        } label: {
-                            buttons[counter].title.text
+                    VStack(spacing: 0) {
+                        alertModel?.title.content
+                        if let subTitle = alertModel?.subTitle {
+                            Text(subTitle)
+                                .padding(.top, 5)
+                                .font(.system(size: 13))
+                                .foregroundColor(.primary.opacity(0.5))
                                 .multilineTextAlignment(.center)
-                                .frame(width: 270, height: 44)
                         }
-                        .buttonStyle(RtBackgroundAnimatedButtonStyle(pressedColor: Color.RtColors.rtOtherSelected))
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.top, 19)
+                    .padding(.bottom, 17)
+                    if let buttons = alertModel?.buttons {
+                        ForEach(0..<buttons.count, id: \.self) { counter in
+                            Divider()
+                                .overlay(Color.RtColors.rtIosElementsAlertSeparator)
+                            Button {
+                                buttons[counter].action?()
+                                alertModel = nil
+                            } label: {
+                                buttons[counter].title.text
+                                    .multilineTextAlignment(.center)
+                                    .frame(width: 270, height: 44)
+                            }
+                            .buttonStyle(RtBackgroundAnimatedButtonStyle(pressedColor: Color.RtColors.rtOtherSelected))
+                        }
                     }
                 }
+                .background(Color.RtColors.rtIosElementsAlertSurface)
+                .background(.ultraThinMaterial)
+                .cornerRadius(15)
+                .frame(width: 270)
+                .transition(.scale(scale: 0.75).combined(with: .opacity).animation(.easeOut(duration: 0.24)))
+                .zIndex(.greatestFiniteMagnitude)
             }
-            .background(Color.RtColors.rtIosElementsAlertSurface)
-            .background(.ultraThinMaterial)
-            .cornerRadius(15)
-            .opacity(alertModel == nil ? 0 : 1)
-            .frame(width: 270)
         }
         .edgesIgnoringSafeArea(.all)
         .zIndex(Double.greatestFiniteMagnitude)
